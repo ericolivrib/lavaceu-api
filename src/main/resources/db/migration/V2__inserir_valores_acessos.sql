b85aff2688bd8b4ea91ef9824305ab917f728691
Érico Ribeiro <oliv.ericorib@gmail.com>
2024-11-15 15:55:43 -0300

    feat: migration de valores para `public.acessos`

---
+++ src/main/resources/db/migration/V2__inserir_valores_acessos.sql
@@ -0,0 +1,8 @@
+insert into acessos (acesso_id, nome)
+values (1, 'admin');
+
+insert into acessos (acesso_id, nome)
+values (2, 'morador');
+
+insert into acessos (acesso_id, nome)
+values (3, 'bolsista');
