df4ee4042d0912a2cf931ad8ef84708c228db3cc
Érico Ribeiro <oliv.ericorib@gmail.com>
2024-11-14 22:13:46 -0300

    feat: iniciar repositório

---
+++ src/main/java/br/com/erico/lavanderia/LavaceuApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.erico.lavanderia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavaceuApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LavaceuApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/erico/lavanderia/LavaceuApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erico.lavanderia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavaceuApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
