b5fd2115f30ac16ba39f379c8c2a13a11ebab82f
Érico Ribeiro <oliv.ericorib@gmail.com>
2024-11-23 23:11:32 -0300

    fix: subir método de busca de usuários por id de acesso que esqueci hà 2 commits atrás =/

--- src/main/java/br/com/erico/lavanderia/model/usuario/UsuarioRepository.java
+++ src/main/java/br/com/erico/lavanderia/model/usuario/UsuarioRepository.java
@@ -1,8 +1,12 @@
 package br.com.erico.lavanderia.model.usuario;
 
+import br.com.erico.lavanderia.dto.UsuarioProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -15,4 +19,12 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     boolean existsByMatricula(String matricula);
 
     boolean existsByEmail(String email);
+
+    @Query(value = """
+            select u.usuario_id as id, u.nome, u.email, u.telefone, u.matricula, u.apartamento, a.nome as acesso
+            from usuarios u, acessos_usuarios au, acessos a
+            where au.usuario_id = u.usuario_id
+            and au.acesso_id = a.acesso_id
+            and a.acesso_id = :acessoId""", nativeQuery = true)
+    List<UsuarioProjection> findByAcessoId(@Param("acessoId") long acessoId);
 }
