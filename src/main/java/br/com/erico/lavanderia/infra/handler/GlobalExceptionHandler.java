528cff1f0055a9c3f02c9c19587042438bbfe7e8
Érico Ribeiro <oliv.ericorib@gmail.com>
2024-12-13 18:04:59 -0300

    feat: handler para ConstrainViolationException

--- src/main/java/br/com/erico/lavanderia/infra/handler/GlobalExceptionHandler.java
+++ src/main/java/br/com/erico/lavanderia/infra/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package br.com.erico.lavanderia.infra.handler;
 
 import br.com.erico.lavanderia.dto.FieldErrorDto;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,6 +16,16 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<FieldErrorDto>> handleFieldErrors(MethodArgumentNotValidException e) {
         List<FieldErrorDto> errors = e.getFieldErrors()
+                .stream()
+                .map(FieldErrorDto::new)
+                .toList();
+
+        return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<List<FieldErrorDto>> handleConstraintViolation(ConstraintViolationException e) {
+        List<FieldErrorDto> errors = e.getConstraintViolations()
                 .stream()
                 .map(FieldErrorDto::new)
                 .toList();
